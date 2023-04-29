ecbd3f976a129191a67307fdc2fe3f71198dee35
linweiyuan <root@linweiyuan.com>
2023-04-29 21:53:33 +0800

    remove confused log

--- middleware/headercheck.go
+++ middleware/headercheck.go
@@ -5,14 +5,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/linweiyuan/go-chatgpt-api/api"
-	"github.com/linweiyuan/go-chatgpt-api/util/logger"
 )
 
 //goland:noinspection GoUnhandledErrorResult
 func HeaderCheckMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if c.GetHeader(api.AuthorizationHeader) == "" && c.Request.URL.Path != "/auth/login" {
-			logger.Info("Missing access token")
 			c.AbortWithStatusJSON(http.StatusForbidden, api.ReturnMessage("Missing accessToken."))
 			return
 		}
