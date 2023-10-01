3029d78fbf568d83ae25c11c99ab4c356a4dedb7
moeakwak <moeakwak@gmail.com>
2023-10-02 01:06:59 +0800

    support multimodal_text content and attachments

--- api/chatgpt/typings.go
+++ api/chatgpt/typings.go
@@ -27,14 +27,15 @@ func (c *CreateConversationRequest) AddMessage(role string, content string) {
 	c.Messages = append(c.Messages, Message{
 		ID:      uuid.New().String(),
 		Author:  Author{Role: role},
-		Content: Content{ContentType: "text", Parts: []string{content}},
+		Content: Content{ContentType: "text", Parts: []interface{}{content}},
 	})
 }
 
 type Message struct {
-	Author  Author  `json:"author"`
-	Content Content `json:"content"`
-	ID      string  `json:"id"`
+	Author   Author      `json:"author"`
+	Content  Content     `json:"content"`
+	ID       string      `json:"id"`
+	Metadata interface{} `json:"metadata"`
 }
 
 type Author struct {
@@ -42,8 +43,8 @@ type Author struct {
 }
 
 type Content struct {
-	ContentType string   `json:"content_type"`
-	Parts       []string `json:"parts"`
+	ContentType string        `json:"content_type"`
+	Parts       []interface{} `json:"parts"`
 }
 
 type CreateConversationResponse struct {
